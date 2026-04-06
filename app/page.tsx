46d67e79ab5f805d9e1ce2d3ae2a1f062dabd83f
mafutka <mafutka@gmail.com>
2026-04-06 22:54:29 +0300

    form-styles-created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 
-
+import RegisterForm from "@/components/Auth/RegisterForm";
 export default function Home() {
   return (
     <div >
       <main>
         <p>HomePage</p>
+        <RegisterForm />
       </main>
     </div>
   );
