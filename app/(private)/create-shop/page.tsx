0a844bf86cec3a6b0cd40a35ab64afd7ef99b24f
mafutka <mafutka@gmail.com>
2026-06-05 22:48:07 +0300

    Footer-imported-shop

--- app/(private)/create-shop/page.tsx
+++ app/(private)/create-shop/page.tsx
@@ -1,9 +1,12 @@
 import CreateShopForm from "@/components/CreateShop/CreateShopForm";
+import Footer from "@/components/Footer/Footer";
 
 export default function CreateShopPage() {
   return (
     <main>
       <CreateShopForm />
+      <Footer/>
     </main>
+
   );
 }
