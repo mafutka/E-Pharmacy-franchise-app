58ceae54535276aaf9da2db2d679f08f725c6890
mafutka <mafutka@gmail.com>
2026-06-21 23:18:16 +0300

    Header-modified

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,19 @@
 import Logo from "../Logo/Logo"
 import scss from "./Header.module.scss"
 
-export default function Header() {
-    return (
-        <header className={scss.header}>
-            <Logo></Logo>
-        </header>
-    )
+export default function Header({ isAuth }: { isAuth: boolean }) {
+  return (  
+    <header>
+      <Logo />
+
+      {isAuth && (
+        <nav  className={scss.nav}>
+          <a>Shop</a>
+          <a>Medicines</a>
+          <a>Statistics</a>
+          <button>Logout</button>
+        </nav>
+      )}
+    </header>
+  )
 }
