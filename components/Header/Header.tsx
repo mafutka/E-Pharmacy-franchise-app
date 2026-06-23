3e8e7aff474e5474a1b658e0a1fa5b639a83306d
mafutka <mafutka@gmail.com>
2026-06-23 20:48:58 +0300

    header-mobile-crated

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,30 @@
+"use client"
+
+import { useState } from "react"
 import Logo from "../Logo/Logo"
 import scss from "./Header.module.scss"
 
 export default function Header({ isAuth }: { isAuth: boolean }) {
-  return (  
+  const [isOpen, setIsOpen] = useState(false)
+  return (
     <header>
       <Logo />
-
+     
       {isAuth && (
-        <nav  className={scss.nav}>
+        <>
+         <button
+            className={`${scss.burger} ${isOpen ? scss.open : ""}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            ☰
+          </button>
+        <nav className={scss.nav}>
           <a>Shop</a>
           <a>Medicines</a>
           <a>Statistics</a>
           <button>Logout</button>
         </nav>
+        </>
       )}
     </header>
   )
