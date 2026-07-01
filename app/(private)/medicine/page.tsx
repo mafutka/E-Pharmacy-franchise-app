80e17a554f72a27c1076b8b0f8f33890b2a9ddf1
mafutka <mafutka@gmail.com>
2026-07-01 06:57:26 +0200

    sprite-updated

--- app/(private)/medicine/page.tsx
+++ app/(private)/medicine/page.tsx
@@ -1,5 +1,10 @@
 export default function MedicinePage() {
-    return (
-        <div>Medicine Page</div>
-    )
+  return (
+    <div>
+      Medicine Page
+      <svg width="40" height="40">
+        <use href="/symbol-defs.svg#icon-coins" />
+      </svg>
+    </div>
+  )
 }
