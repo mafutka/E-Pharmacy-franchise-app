3a18993f24e2b94ed24fe20bf49119d5d6ab0294
mafutka <mafutka@gmail.com>
2026-07-06 14:40:18 +0300

    homepage-edited

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { redirect } from "next/navigation"
-import RegiscterPage from "./(auth)/register/page";
 export default function Home() {
   const isAuth = false;
 
   if (isAuth) {
-    redirect("/shop")
+    redirect("/shop");
+  } else {
+    redirect("/register");
   }
-  return (
-    <div>
-      <RegiscterPage/>
-    </div>
-  )
+
 }
