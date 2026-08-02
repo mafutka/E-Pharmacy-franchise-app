3bddb51804f2d5f79c7179d9ee6d48672b87d617
mafutka <mafutka@gmail.com>
2026-08-02 22:06:59 +0300

    all-medicines-fixed

--- components/MedicineCard/MedicineCard.tsx
+++ components/MedicineCard/MedicineCard.tsx
@@ -10,6 +10,7 @@ export default function MedicineCard({ product, onDetails }: Props) {
     <div>
       <h4>{product.name}</h4>
       <p>{product.price}$</p>
+    
 
       {onDetails && (
         <button onClick={onDetails}>Details</button>
