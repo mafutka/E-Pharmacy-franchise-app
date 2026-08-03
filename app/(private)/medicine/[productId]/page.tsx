5ea1319f7fc5d18daa037472d978a87ab4cd9ab8
mafutka <mafutka@gmail.com>
2026-08-03 14:39:53 +0300

    productId-page-added

---
+++ app/(private)/medicine/[productId]/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useParams } from "next/navigation"
+import { getOneProduct } from "@/services/productApi"
+import { Product } from "@/types/shop"
+
+export default function ProductDetailsPage() {
+  const { productId } = useParams()
+
+  const [product, setProduct] = useState<Product | null>(null)
+
+  useEffect(() => {
+    if (!productId) return
+
+    getOneProduct(productId as string).then(setProduct)
+  }, [productId])
+
+  if (!product) return <p>Loading...</p>
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+
+      {product.image && (
+        <img
+          src={`${process.env.API_URL}${product.image}`}
+          alt={product.name}
+          width={200}
+        />
+      )}
+
+      <p>Price: {product.price}$</p>
+      <p>Brand: {product.brand}</p>
+      <p>Stock: {product.stock}</p>
+
+      {/* поки нема description */}
+      <p>Description: No description yet</p>
+
+      {/* поки нема reviews */}
+      <p>Reviews: No reviews yet</p>
+    </div>
+  )
+}
--- components/MedicineCard/MedicineCard.tsx
+++ components/MedicineCard/MedicineCard.tsx
@@ -8,6 +8,13 @@ type Props = {
 export default function MedicineCard({ product, onDetails }: Props) {
   return (
     <div>
+         {product.image && (
+        <img
+          src={`${process.env.API_URL}${product.image}`}
+          alt={product.name}
+          width={100}
+        />
+      )}
       <h4>{product.name}</h4>
       <p>{product.price}$</p>
     
