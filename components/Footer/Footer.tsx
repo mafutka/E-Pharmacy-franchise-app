c82af2545f248a14c81a8ef95e8fa5624b225728
mafutka <mafutka@gmail.com>
2026-09-06 21:49:24 +0300

    footer-navigation

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,41 @@
 import Logo from "../Logo/Logo"
 import scss from "./Footer.module.scss"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Footer() {
+    const pathname = usePathname()
   return (
     <footer className={scss.footer}>
       <div className={scss.container}>
         <Logo variant="footer" />
 <p>Get the medicine to help you feel better, get back to your active life, and enjoy every moment.</p>
-        {/* тут потім navigation */}
+         <div className={scss.menuGroup}>
+                      <Link
+                        className={`${scss.navBtn} ${
+                          pathname.startsWith("/medicine") ? scss.active : ""
+                        }`}
+                        href="/medicine"
+                      >
+                        <span>Medicines</span>
+                      </Link>
+                      <Link
+                        className={`${scss.navBtn} ${
+                          pathname.startsWith("/shop") ? scss.active : ""
+                        }`}
+                        href="/shop"
+                      >
+                        <span>Shop</span>
+                      </Link>
+                      <Link
+                        className={`${scss.navBtn} ${
+                          pathname.startsWith("/statistics") ? scss.active : ""
+                        }`}
+                        href="/statistics"
+                      >
+                        <span>Statistics</span>
+                      </Link>
+                    </div>
       </div>
     </footer>
   )
