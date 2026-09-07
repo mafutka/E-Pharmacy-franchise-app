1524e13c64612bab6f8ce3983e05efd88fbf26d0
mafutka <mafutka@gmail.com>
2026-09-07 20:44:17 +0300

    footer-mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,38 +4,48 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 export default function Footer() {
-    const pathname = usePathname()
+  const pathname = usePathname()
   return (
     <footer className={scss.footer}>
       <div className={scss.container}>
-        <Logo variant="footer" />
-<p>Get the medicine to help you feel better, get back to your active life, and enjoy every moment.</p>
-         <div className={scss.menuGroup}>
-                      <Link
-                        className={`${scss.navBtn} ${
-                          pathname.startsWith("/medicine") ? scss.active : ""
-                        }`}
-                        href="/medicine"
-                      >
-                        <span>Medicines</span>
-                      </Link>
-                      <Link
-                        className={`${scss.navBtn} ${
-                          pathname.startsWith("/shop") ? scss.active : ""
-                        }`}
-                        href="/shop"
-                      >
-                        <span>Shop</span>
-                      </Link>
-                      <Link
-                        className={`${scss.navBtn} ${
-                          pathname.startsWith("/statistics") ? scss.active : ""
-                        }`}
-                        href="/statistics"
-                      >
-                        <span>Statistics</span>
-                      </Link>
-                    </div>
+        <div className={scss.topFooter}>
+          <Logo variant="footer" />
+          <p className={scss.footerText}>
+            Get the medicine to help you feel better, get back to your active
+            life, and enjoy every moment.
+          </p>
+        </div>
+        <div className={scss.bottomFooter}>
+          <div className={scss.menuGroup}>
+            <Link
+              className={`${scss.navBtn} ${
+                pathname.startsWith("/medicine") ? scss.active : ""
+              }`}
+              href="/medicine"
+            >
+              <span>Medicines</span>
+            </Link>
+            <Link
+              className={`${scss.navBtn} ${
+                pathname.startsWith("/shop") ? scss.active : ""
+              }`}
+              href="/shop"
+            >
+              <span>Shop</span>
+            </Link>
+            <Link
+              className={`${scss.navBtn} ${
+                pathname.startsWith("/statistics") ? scss.active : ""
+              }`}
+              href="/statistics"
+            >
+              <span>Statistics</span>
+            </Link>
+          </div>
+          <div className={scss.mediia}>
+            <p>Icons</p>
+          </div>
+        </div>
       </div>
     </footer>
   )
